c3b8282a66f4d11f1178199a03315fb1045f97cd
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-19 10:33:12 -0700

    Code splitting - method 3 (lazy)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
 
 class App extends Component {
 	constructor() {
@@ -13,18 +12,25 @@ class App extends Component {
 	}
 
 	onRouteChange = route => {
-		// With Code Splitting
 		this.setState({ route });
 	};
 	render() {
 		if (this.state.route === 'page1') {
 			return <Page1 onRouteChange={this.onRouteChange} />;
 		} else if (this.state.route === 'page2') {
-			const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
-			return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+			const Page2 = React.lazy(() => import('./components/Page2'));
+			return (
+				<Suspense fallback={<div>Loading...</div>}>
+					<Page2 onRouteChange={this.onRouteChange} />
+				</Suspense>
+			);
 		} else if (this.state.route === 'page3') {
-			const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
-			return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+			const Page3 = React.lazy(() => import('./components/Page3'));
+			return (
+				<Suspense fallback={<div>Loading...</div>}>
+					<Page3 onRouteChange={this.onRouteChange} />
+				</Suspense>
+			);
 		}
 	}
 }
